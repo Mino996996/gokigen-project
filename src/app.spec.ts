6a06095bbe1a91c9560cae6ac1750e9d1faa9d20
Mino996996 <minoruaka@gmail.com>
2021-03-13 15:49:50 +0900

    refine: テストコードにエラーを出すことを追加

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,21 +1,26 @@
-import { App } from "./app";
+import { App } from './app';
 
-class AppImpl extends App {
-}
-
-describe("App", () => {
-  it("should works without error", () => {
-    expect(() => new App()).not.toThrowError();
-  })
+describe('App', () => {
+  it('create an App class', () => {
+    expect(() => new App()).toBeDefined();
+  });
 
   test('私の気分を記録して欲しい', () => {
     // test here
-    const app = new App()
-    app.recordMood("good")
-    expect(app.todaysMood()).toBe("good")
-  })
+    const app = new App();
+    app.recordMood('good');
+    expect(app.todaysMood()).toBe(3);
+  });
 
-  it('store some data when click buttons', () => {
-    // test here
-  })
-})
+  it('is added other word or number', () =>{
+    const app = new App();
+    expect(app.recordMood('happy!')).toBe('error!');
+  });
+
+  it('calculates average in a days ', () => {
+    const feelings = [3, 1, 2, 3, 3];
+    const app = new App();
+    app.calculateAve(feelings);
+    expect(app.calculateAve(feelings)).toBe(2.4);
+  });
+});
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,36 @@
-export class App {
-  recordMood(good: string) {
+import { User } from './type-mosels';
 
+export class App {
+  user: User = {
+    feelingList: [],
+  };
+
+  recordMood(feel: string): void | string {
+    switch (feel) {
+      case 'good':
+        this.user.feelingList.push(3);
+        break;
+      case 'soso':
+        this.user.feelingList.push(2);
+        break;
+      case 'bad':
+        this.user.feelingList.push(1);
+        break;
+      default:
+        return 'error!';
+        break;
+    }
   }
 
-  todaysMood(): string {
-    throw new Error("not implemented");
+  todaysMood(): number {
+    return this.user.feelingList.slice(-1)[0];
+  }
+
+  calculateAve(feelings: number[]): number {
+    let sum = 0;
+    feelings.forEach((value) => {
+      sum += value;
+    });
+    return sum / feelings.length;
   }
 }
