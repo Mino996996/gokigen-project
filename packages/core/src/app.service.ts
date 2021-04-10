f1a03f608671cfdc10ce8054aa6f541a3dd6bc16
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-04-10 16:57:12 +0900

    feat(core): add demo api for web

--- packages/core/src/app.service.ts
+++ packages/core/src/app.service.ts
@@ -2,7 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  connectionDemo(): { name: string; birth: Date }[] {
+    return [
+      {
+        name: 'John',
+        birth: new Date('2000-04-10T07:54:43.547Z'),
+      },
+      {
+        name: 'Smith',
+        birth: new Date('2000-04-01T07:54:43.547Z'),
+      },
+    ];
   }
 }
