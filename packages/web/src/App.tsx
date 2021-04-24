c46d4fedc6de153b371b5a8cc0539b46379c71f3
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-04-23 22:51:21 -1000

    feat(web): fetch user on load

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ExternalTextLink} from "./coponents/atoms/externalTextLink/externalTextLink";
@@ -7,10 +7,7 @@ import axios, {AxiosResponse} from "axios";
 type User = { name: string; birth: string };
 
 function App() {
-  const [user, setUser]: [User, ((value: (((prevState: User) => User) | User)) => void)] = useState<User>({
-    name: 'Initial',
-    birth: 'null'
-  });
+  const [user, setUser]: [User | null, ((value: (((prevState: User|null) => User) | User)) => void)] = useState<User | null>(null);
 
   const getNetworkUser = () => axios.get("http://localhost:3001/")
     .then((res: AxiosResponse<Array<User>>) => setUser(res.data[0]))
@@ -18,16 +15,26 @@ function App() {
       setUser({name: 'Network Error', birth: 'error'})
     })
 
+  useEffect(() => {
+    (async () => {
+      await getNetworkUser()
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" className="h-72 pointer-events-none"/>
-        <p>
-          {user.name}
-        </p>
-        <p>
-          {user.birth}
-        </p>
+        {user &&
+          <>
+            <p>
+              {user.name}
+            </p>
+            <p>
+              {user.birth}
+            </p>
+          </>
+        }
         <button onClick={getNetworkUser}>Click to Update User</button>
         <ExternalTextLink text={"Learn React"} href={"https://reactjs.org"}/>
       </header>
