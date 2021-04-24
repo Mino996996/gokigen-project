35cc75f36c66e53559c198ad8b7f02947939f096
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-04-23 22:53:58 -1000

    refactor(web): use useAsync instead of useEffect

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,13 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ExternalTextLink} from "./coponents/atoms/externalTextLink/externalTextLink";
 import axios, {AxiosResponse} from "axios";
+import {useAsync} from "react-use";
 
 type User = { name: string; birth: string };
 
 function App() {
-  const [user, setUser]: [User | null, ((value: (((prevState: User|null) => User) | User)) => void)] = useState<User | null>(null);
+  const [user, setUser]: [User | null, ((value: (((prevState: User | null) => User) | User)) => void)] = useState<User | null>(null);
 
   const getNetworkUser = () => axios.get("http://localhost:3001/")
     .then((res: AxiosResponse<Array<User>>) => setUser(res.data[0]))
@@ -15,10 +16,8 @@ function App() {
       setUser({name: 'Network Error', birth: 'error'})
     })
 
-  useEffect(() => {
-    (async () => {
-      await getNetworkUser()
-    })()
+  useAsync(async () => {
+    await getNetworkUser()
   }, [])
 
   return (
@@ -26,14 +25,14 @@ function App() {
       <header className="App-header">
         <img src={logo} alt="logo" className="h-72 pointer-events-none"/>
         {user &&
-          <>
-            <p>
-              {user.name}
-            </p>
-            <p>
-              {user.birth}
-            </p>
-          </>
+        <>
+          <p>
+            {user.name}
+          </p>
+          <p>
+            {user.birth}
+          </p>
+        </>
         }
         <button onClick={getNetworkUser}>Click to Update User</button>
         <ExternalTextLink text={"Learn React"} href={"https://reactjs.org"}/>
