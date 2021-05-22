1b23232bda21ba5808caefddc5de257ddcd169ce
Mino996996 <minoruaka@gmail.com>
2021-05-22 16:00:45 +0900

    wip: テストの作成中

---
+++ packages/core/src/radio.spec.ts
@@ -0,0 +1,11 @@
+describe('RadioScript', ()=>{
+  it('create an RadioScript class', () =>{
+    expect(()=> new RadioScript()).toBeDefined();
+  });
+
+  it('経過時間を表示する', () =>{
+    const startTime =''; //date(~~~);
+    // 予定時間が一致したらtrueを返すとかそんな感じ
+  });
+
+})
