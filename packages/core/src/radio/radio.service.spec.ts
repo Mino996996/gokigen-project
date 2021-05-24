85f5ff3c096cac605a13e8a5949c0b30993207b0
Mino996996 <minoruaka@gmail.com>
2021-05-25 05:52:29 +0900

    wip: サービスのテストを作成中

--- packages/core/src/radio/radio.service.spec.ts
+++ packages/core/src/radio/radio.service.spec.ts
@@ -1,6 +1,23 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { RadioService } from "./radio.service";
 
+type ScriptItems = {
+  contentType: ContentType;
+  corner:string;
+  talkOutline:string;
+  mainGive:string;
+  time:number;
+}
+
+enum ContentType {
+  'opening',
+  'freeTalk',
+  'jingle',
+  'miniCorner',
+  'corner',
+  'music',
+  'ending'
+}
 describe('RadioService', () => {
   let service: RadioService;
 
@@ -10,6 +27,10 @@ describe('RadioService', () => {
     }).compile();
 
     service = module.get<RadioService>(RadioService);
+  });
+
+  test('コンテンツリストで0を選択したらopeningを返す', ()=>{
+    expect(service.).toEqual('opening');
   });
 
   test('経過時間を計算する', () =>{
