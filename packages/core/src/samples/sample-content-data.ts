c3cf69df0fdf82c49ca8f81d4482d57988973298
Mino996996 <minoruaka@gmail.com>
2021-06-19 13:27:32 +0900

    feat: テスト用のサンプルデータを作成

---
+++ packages/core/src/samples/sample-content-data.ts
@@ -0,0 +1,19 @@
+const sampleContent = [
+  {
+    id:1, date:'2021/5/30 12:00', contentType:1, order:1, time:5, bgm:'春の海',
+    talkTheme:'餃子の話', talkScript:'美味しい焼き方おしえて', mainSpeaker:[1],
+    memo:'テンション高めに', groupId:1
+  },
+  {
+    id:2, date:'2021/6/1 13:00', contentType:1, order:1, time:5, bgm:'エーデルワイス',
+    talkTheme:'みかんの話', talkScript:'皮が厚いとおもったら伊予柑だった', mainSpeaker:[],
+    memo:'テンション高めに', groupId:1
+  },
+  {
+    id:3, date:'2021/7/1 13:00', contentType:3, order:4, time:40,
+    talkTheme:'ふつおた', mainSpeaker:[3,4], groupId:2
+  },
+  {
+    id:4, date:'2021/6/1 13:00', contentType:4, order:3, groupId:1
+  },
+];
---
+++ packages/core/src/samples/sample-group-data.ts
@@ -0,0 +1,4 @@
+const sampleGroup= [
+  {id: 1, name:'電波ガレージ', member:[1,2]},
+  {id: 2, name:'ラフメイカーズラジオ', member: [3,4]}
+]
