07ddbf027df73e96f04a6c3bf2accc6c73e09dd5
Mino996996 <minoruaka@gmail.com>
2021-07-17 15:52:25 +0900

    update(fixture): add data for test case.

--- packages/core/src/radio/fixtures/sample-group-data.ts
+++ packages/core/src/radio/fixtures/sample-group-data.ts
@@ -1,4 +1,10 @@
 export const sampleGroup= [
   {id: 1, name:'電波ガレージ', member:[1,2]},
-  {id: 2, name:'ラフメイカーズラジオ', member: [3,4]}
+  {id: 2, name:'ラフメイカーズラジオ', member: [3,4]},
+  {id: 3, name:'ocean wave radio', member:[1,5]}
+]
+
+export const okGroup = [
+  {id: 1, name:'電波ガレージ', member:[1,2]},
+  {id: 3, name:'ocean wave radio', member:[1,5]}
 ]
--- packages/core/src/radio/fixtures/sample-user-data.ts
+++ packages/core/src/radio/fixtures/sample-user-data.ts
@@ -1,6 +1,7 @@
 export const sampleUser = [
-  { id: 1, name: "aka", pass: "akaaka" },
-  { id: 2, name: "mino", pass: "passpass" },
-  { id: 3, name: "aaa", pass: "asdf" },
-  { id: 4, name: "bbb", pass: "pass1234" }
+  { id: 1, name: "aka", pass: "akaaka", status: 'login' },
+  { id: 2, name: "mino", pass: "passpass", status: 'login' },
+  { id: 3, name: "aaa", pass: "asdf", status: 'login' },
+  { id: 4, name: "bbb", pass: "pass1234", status: 'logout' },
+  { id: 5, name: "jake", pass: "jacky", status: 'logout'}
 ];
