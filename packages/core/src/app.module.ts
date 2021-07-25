0fdd7c93cb0012d0f0071e748b9367e052b9880a
Mino996996 <minoruaka@gmail.com>
2021-07-25 22:34:04 +0900

    feature(radio): install npm postgresql library

--- packages/core/src/app.module.ts
+++ packages/core/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RadioModule } from './radio.module';
+import { PostgresqlService } from './postgresql/postgresql.service';
 
 @Module({
   imports: [RadioModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostgresqlService],
 })
 export class AppModule {}
